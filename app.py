11ac27f256ef56fe83f806d93744a4daa537d4ca
Victoria Schweiger <134645175+vicschweiger@users.noreply.github.com>
2026-04-07 16:37:01 +0000

    refatora a conexão com o banco de dados e melhora as mensagens de erro na rota POST /ips; adiciona .env ao .gitignore

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import os
 import requests
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client.dimensa_project
+ips_collection = db.ips
 
 app = Flask(__name__)
 
@@ -10,14 +19,16 @@ def receive_ip():
     received_ip = query.get('ip')
 
     if not query:
-        return jsonify({"erro": "Corpo da requisição vazio ou Content-Type incorreto. Envie um JSON."}), 400
+        return jsonify({"erro": "Corpo da requisição vazio."}), 400
         
     
     if not received_ip:
-        return jsonify({"erro": "O campo 'ip' é obrigatório"}), 400
+        return jsonify({"erro": "O campo 'ip' está vazio."}), 400
+    
+    ips_collection.insert_one({"ip": received_ip})  
 
     return jsonify({
-        "mensagem": "Sucesso! A rota está funcionando.",
+        "mensagem": "Sucesso! O banco de dados foi conectado e salvou o IP corretamente.",
         "ip_que_voce_enviou": received_ip
     }), 200
 
